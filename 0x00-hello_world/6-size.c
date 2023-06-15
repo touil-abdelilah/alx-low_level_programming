f3bb090b272603422c5130456632b91c80953133
Abdelilah <et.touil505@gmail.com0>
2023-06-15 15:14:45 -0700

    added

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,6 +14,6 @@ int main(void)
 	printf("Size of a long int: %lu byte(s)\n", sizeof(long));
 	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long));
 	printf("Size of a float: %lu byte(s)\n", sizeof(float));
-	return(0);
+	return (0);
 }
 
