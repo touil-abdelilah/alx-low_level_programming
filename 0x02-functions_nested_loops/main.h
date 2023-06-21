6f56132ba29ac1b29cf862787f7d1c5de2d96c3d
Abdelilah <et.touil505@gmail.com0>
2023-06-20 19:03:39 -0700

    done

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -14,4 +14,7 @@ int _isalpha(int c);
 int print_sign(int n);
 int _abs(int n);
 int print_last_digit(int n);
+void jack_bauer(void);
+void jack_bauer(void);
+int add(int a, int b);
 #endif /* MAIN_H */
