374922a2591c4abd98d06fc1926c27f400bbad9c
Abdelilah <et.touil505@gmail.com0>
2023-06-20 17:07:59 -0700

    added

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include"main.h"
+/*
+ *
+ * main - entry level of the programme
+ *
+ * print_alphabe - print the alphapets in lower case
+ *
+ *
+ * return 0 (success)
+ **/
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
+
+void print_alphabet()
+{
+	char c;
+	for(c = 'a'; c <= 'z'; c++)
+	putchar ("%c",c);
+}
+
