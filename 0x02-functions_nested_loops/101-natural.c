520045b4afb3f1d7b43a3416d5dfda7a374998f9
Abdelilah <et.touil505@gmail.com0>
2023-06-20 20:02:36 -0700

    m

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -7,20 +7,18 @@
  */
 int main(void)
 {
-    int limit = 1024;  /* Define the upper limit */
-    int sum = 0;  /* Initialize the sum variable */
-    int i;  /* Declare the loop counter variable */
+	int limit = 1024; /* Define the upper limit */
+	int sum = 0; /* Initialize the sum variable */
+	int i; /* Declare the loop counter variable */
 
-    for (i = 0; i < limit; i++)
-    {
-        if (i % 3 == 0 || i % 5 == 0)
-        {
-            sum += i;  /* Add the current number to the sum if divisible by 3 or 5 */
-        }
-    }
+	for (i = 0; i < limit; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+			sum += i; /* Add the current number to the sum if divisible by 3 or 5 */
+	}
 
-    printf("%d\n", sum);  /* Print the sum */
+	printf("%d\n", sum); /* Print the sum */
 
-    return 0;
+	return (0);
 }
 
