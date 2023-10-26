23103fc7b3d9fcbee29c33de0998da031b7ca45c
touil-abdelilah <et.touil505@gmail.com>
2023-10-25 18:09:07 -0700

    Write a function that prints a string, followed by a new line.

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - Prints a string followed by a new line.
+ * @s: The string to be printed.
+ */
+void _puts_recursion(char *s)
+{
+    if (*s == '\0') // Base case: when the string ends (null terminator).
+    {
+        _putchar('\n'); // Print a newline character to start a new line.
+        return;
+    }
+    
+    _putchar(*s); // Print the current character.
+    _puts_recursion(s + 1); // Recursively call the function for the next character.
+}
