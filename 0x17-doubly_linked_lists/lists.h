fd9b794fbf464f0d2ee7e08fa65e0f1d37970ee9
et.touil <et.touil505@gmail.com>
2024-01-12 00:28:04 +0100

    Write a function that returns the number of elements in a linked dlistint_t list.

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -1,8 +1,8 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-#include <stdio.h>
-#include <stdlib.h>
+#include <stddef.h>
+
 /**
  * struct dlistint_s - doubly linked list
  * @n: integer
@@ -21,5 +21,5 @@ typedef struct dlistint_s
 /* Function prototypes */
 size_t print_dlistint(const dlistint_t *h);
 size_t dlistint_len(const dlistint_t *h);
-#endif /* LISTS_H */
 
+#endif /* LISTS_H */
