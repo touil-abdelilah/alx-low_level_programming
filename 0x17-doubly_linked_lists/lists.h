84cbae9f770427498baf848c4606daad659dc8af
et.touil <et.touil505@gmail.com>
2024-01-11 23:56:06 +0100

    Write a function that prints all the elements of a dlistint_t list.

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -1,6 +1,3 @@
-#ifndef LISTS_H
-#define LISTS_H
-
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -21,5 +18,3 @@ typedef struct dlistint_s
 
 /* Function prototypes */
 size_t print_dlistint(const dlistint_t *h);
-
-#endif /* LISTS_H */
