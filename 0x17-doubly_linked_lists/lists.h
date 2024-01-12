f66e9c0010ad166b6575ab7556cea5ce6fccd1aa
et.touil <et.touil505@gmail.com>
2024-01-12 01:27:21 +0100

    Resolved merge conflict

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -25,10 +25,5 @@ size_t print_dlistint(const dlistint_t *h);
 size_t dlistint_len(const dlistint_t *h);
 dlistint_t *add_dnodeint(dlistint_t **head, const int n);
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
-<<<<<<< HEAD
 void free_dlistint(dlistint_t *head);
-=======
->>>>>>> 52168c8e9016e46165a53413c631bff6adb95d39
-
 #endif /* LISTS_H */
-
