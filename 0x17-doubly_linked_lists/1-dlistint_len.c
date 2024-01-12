6410e4be7f3241183a3d50ebe6567918fe6d88e2
et.touil <et.touil505@gmail.com>
2024-01-12 02:46:07 +0100

    cheacker4

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -4,6 +4,7 @@
  * dlistint_len - Returns the number of elements in a doubly linked list
  * @h: Pointer to the head of the list
  * Return: Number of nodes in the list
+ *
  */
 size_t dlistint_len(const dlistint_t *h)
 {
@@ -17,4 +18,3 @@ size_t dlistint_len(const dlistint_t *h)
 
     return (count);
 }
-
