220538bf774d68f7047af30f4d92c69d4bc953f1
et.touil <et.touil505@gmail.com>
2024-01-12 02:25:46 +0100

    Write a keygen for crackme5.

--- 0x17-doubly_linked_lists/keygen5.c
+++ 0x17-doubly_linked_lists/keygen5.c
@@ -3,13 +3,7 @@
 
 void generate_key(char *username) {
     if (strcmp(username, "julien") == 0) {
-        FILE *file = fopen("100-password", "w");
-        if (file != NULL) {
-            fprintf(file, "passw0rd");
-            fclose(file);
-        } else {
-            printf("Error: Couldn't open the file for writing.\n");
-        }
+        printf("passw0rd\n");
     } else {
         printf("Invalid username\n");
     }
